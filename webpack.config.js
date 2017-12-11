7412874215960d8e38b20bb636d2887271df592b
sparcut <sparcut@users.noreply.github.com>
2017-12-11 16:27:23 +1100

    added webpack & general site structure

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// Packages
+const webpack = require('webpack'),
+      path = require('path');
+
+// Webpack Plugins
+const CopyPlugin = require('copy-webpack-plugin');
+
+// Paths
+const src = path.join(__dirname, 'src'),
+      dist = path.join(__dirname, 'dev-build');
+
+module.exports = {
+  resolve: {
+    alias: {
+      src,
+    }
+  },
+  context: src,
+  entry: {
+    admin: './admin/',
+    index: './index.js'
+  },
+  output: {
+    path: dist,
+    filename: './[name].js'
+  },
+
+  module: {},
+
+  plugins: [
+    new CopyPlugin([
+      '**/*.html',
+    ])
+  ],
+
+  devtool: '#inline-cheap-source-map'
+}
